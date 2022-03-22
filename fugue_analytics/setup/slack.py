41af016b03e3b42b507a2e3041ba2d7261a5b1a3
Chris Santiago <chrisantiago217@gmail.com>
2022-03-22 13:34:53 -0400

    Update values

--- fugue_analytics/setup/slack.py
+++ fugue_analytics/setup/slack.py
@@ -6,10 +6,10 @@ def initialize_slack_stats():
     """
     Inserts initial Slack channel joins
     """
-    df = pd.DataFrame({"datetime": pd.to_datetime(["2020-02-24"]),
-                       "source": ["Slack Channel Joins"],
-                       "value": [105]})
-
+    df = pd.DataFrame({"datetime": pd.to_datetime(["2020-02-24", "2022-03-22"]),
+                       "source": ["Slack Channel Joins", "Slack Channel Joins"],
+                       "value": [105, 116]})
+    
     insert_df_to_table(df, "metrics_over_time")
     print("Successfully Inserted Slack Channel Joins!")
     return
