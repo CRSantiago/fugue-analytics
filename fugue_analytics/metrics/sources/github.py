dd283c31d213111ef171d66d546be07a3920af4b
Chris Santiago <chrisantiago217@gmail.com>
2022-03-22 13:46:40 -0400

    remove logger

--- fugue_analytics/metrics/sources/github.py
+++ fugue_analytics/metrics/sources/github.py
@@ -1,4 +1,3 @@
-from prefect import get_run_logger, task, flow
 from fugue_analytics.utilities.postgres import connect_to_postgres
 import httpx
 import pandas as pd
@@ -13,10 +12,6 @@ def get_github_star_count(repo="fugue-project/fugue") -> pd.DataFrame:
     stars = res['stargazers_count']
     forks = res['forks']
 
-    logger = get_run_logger()
-    logger.info(f"There are this many stars: {stars}")
-    logger.info(f"There are this many forks: {forks}")
-
     current_datetime = datetime.now()
     res = pd.DataFrame({"datetime": pd.to_datetime([current_datetime, current_datetime]),
                         "source": ["Fugue Repo Stars", "Fugue Repo Forks"],
@@ -24,7 +19,6 @@ def get_github_star_count(repo="fugue-project/fugue") -> pd.DataFrame:
     return res
 
 if __name__ == "__main__":
-    @flow()
     def github_star_count(repo="fugue-project/fugue"):
         res = get_github_star_count(repo)
 
