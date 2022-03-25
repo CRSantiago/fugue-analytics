8e82330081760e720de16fe16a87eabe3ea51eb8
Chris Santiago <chrisantiago217@gmail.com>
2022-03-25 12:24:32 -0400

    Load GA data to dataframe

--- fugue_analytics/setup/google_analytics.py
+++ fugue_analytics/setup/google_analytics.py
@@ -5,24 +5,57 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import os
 
+# Load in GA credentials from the dotenv file
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 KEY_FILE_LOCATION = 'google_api_key.json'
 VIEW_ID = os.environ['FUGUE-DOCS-VIEWID']
 
+# Set up GA connection
 credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_LOCATION, SCOPES)
-
 analytics = build('analyticsreporting', 'v4', credentials=credentials)
 
+# Query GA API
 response = analytics.reports().batchGet( 
     body={ 
         'reportRequests': [ 
                 { 
                     'viewId': VIEW_ID, 
                     'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}], 
+                    "dimensions": [{'name': 'ga:country'}]
+                },
+                { 
+                    'viewId': VIEW_ID, 
+                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}], 
                     'metrics': [{'expression': 'ga:pageviews'}], 
-                    "dimensions": [{'name': 'ga:country'}, {'name': 'ga:sourceMedium'}]
+                },
+                { 
+                    'viewId': VIEW_ID, 
+                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}], 
+                    "dimensions": [{'name': 'ga:sourceMedium'}]
                 },
             ] 
         }).execute()
 
-print(response)
+df = pd.DataFrame()
+
+# Parse the data and save it do a dataframe
+for report in response.get('reports', []):
+
+    columnHeader = report.get('columnHeader', {})
+    dimensionHeader = columnHeader.get('dimensions', [])
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+   
+    rows = report.get('data', {}).get('rows', [])
+
+    df_columns = dimensionHeader + [head['name'] for head in metricHeaders]
+    df_rows = []
+        
+    for row in rows:
+        dimension = row.get('dimensions', [])
+        dateRangeValues = row.get('metrics', [])[0].get('values', [])
+            
+        df_row = dimension + [int(value) for value in dateRangeValues]
+        df_rows.append(df_row)
+    
+    df[f"{df_columns[0]}"] = pd.Series(df_rows)
+    
